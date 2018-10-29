f6a3bb7164365896cf9876a03b5a5e8cf958a48c
Free8Spirit <davidvert2016@gmail.com>
2018-10-29 08:51:21 -0500

    init commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+console.log("hello world");
+
+const chalk = require('chalk');
+const chalkers = require('chalk-animation');
+var glitch = chalkers.glitch('This is colorful text')
+console.log(chalk.blue('Hello World'));
+
+console.log(glitch.start());
