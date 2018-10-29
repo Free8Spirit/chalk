486b6c7ebe4ddc2d3c8840172a378b85cac2b112
Free8Spirit <davidvert2016@gmail.com>
2018-10-29 09:24:52 -0500

    glitch

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ console.log("hello world");
 
 const chalk = require('chalk');
 const chalkers = require('chalk-animation');
-var glitch = chalkers.glitch('This is colorful text')
+
 console.log(chalk.blue('Hello World'));
 
-console.log(glitch.start());
+var glitch = chalkers.glitch('This is colorful text', 2);
+setTimeout(() => {
+    glitch.stop();
+}, 500);
+setTimeout(() => {
+    glitch.start();
+}, 1000);
